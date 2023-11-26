4d3bd5a184744cd0802b726a356dd7f448180c3e
Vasily Turkin <turkin1051994@gmail.com>
2023-11-26 21:09:36 +0300

    Добавлена запись в файл и генерация ID

--- src/Parser.php
+++ src/Parser.php
@@ -1,46 +1,1 @@
-<?php
-
-namespace Src;
-
-use DiDom\Document;
-use DiDom\Exceptions\InvalidSelectorException;
-
-class Parser
-{
-    public const BASE_URL = 'https://www.imdb.com';
-
-    public const MOVIE_NAME_SELECTOR = '#__next > main > div > section > section > div > section > section > div > div > h1 > span';
-
-    /**
-     * @throws InvalidSelectorException
-     */
-
-    public function run(): void
-    {
-        $movieStartIndex = 1;
-        $movieMaxIndex = 9;
-
-        $movieLink = [];
-
-        $moviesData = [];
-        
-        $start = microtime(true);
-
-        for ($i = $movieStartIndex; $i <= $movieMaxIndex; $i++) {
-            $movieIdGenerate = sprintf('/title/tt%07d/', $i);
-            $movieLink[] = self::BASE_URL . $movieIdGenerate;
-        }
-
-        foreach ($movieLink as $item) {
-            $document = new Document($item, true);
-            $movieElement = $document->find(self::MOVIE_NAME_SELECTOR);
-            
-            foreach ($movieElement as $movie){
-                $movieName = $movie->text();
-                $moviesData[] = [$movieName => $item];
-            }
-        }
-        var_dump($moviesData);
-        echo 'Время выполнения скрипта: '.round(microtime(true) - $start, 4).' сек.';
-    }
-}
+<?phpnamespace Src;use DiDom\Document;use DiDom\Exceptions\InvalidSelectorException;class Parser{    public const BASE_URL = 'https://www.imdb.com/';    public const MOVIE_NAME_SELECTOR = '#__next > main > div > section > section > div > section > section > div > div > h1 > span';    /**     * @throws InvalidSelectorException     */    public function run(): void    {        $movieMaxIndex = 9;        $moviesData = [];        $moviesStorageFile = __DIR__ . '/../data/movies.json';        if (file_exists($moviesStorageFile)) {            $moviesData = json_decode(file_get_contents($moviesStorageFile), true) ?? [];        }        for ($i = 1; $i <= $movieMaxIndex; $i++) {            $movieId = sprintf('%07d', $i);            $match = false;            foreach ($moviesData as $movie) {                if ($movie['sourceId'] === $movieId) {                    $match = true;                    break;                }            }            if ($match) {                continue;            }            $movieLink = self::BASE_URL . 'title/tt' . $movieId;            // Проверка наличия страницы            $content = @file_get_contents($movieLink);            if (!$content) {                continue;            }            $document = new Document($movieLink, true);            $moviesElement = $document->fiпшеnd(self::MOVIE_NAME_SELECTOR);            foreach ($moviesElement as $element) {                $movieName = $element->text();                    $moviesData[] = [                        'sourceId' => $movieId,                        'name' => $movieName,                        'link' => $movieLink                    ];            }            file_put_contents($moviesStorageFile, json_encode($moviesData));            var_dump($moviesData);        }    }}
