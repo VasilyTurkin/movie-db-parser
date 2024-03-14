e10b00b2ed1122da69cd5b2451c191112a535532
Vasily Turkin <turkin1051994@gmail.com>
2024-03-14 20:13:29 +0300

    Создан файл для миграции в базу данных

---
+++ jsonToDb.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Src;
+
+use PDO;
+
+$user = 'root';
+
+$pass = null;
+
+$moviesData = [];
+
+$moviesStorageFile = __DIR__ . '/../data/movies.json';
+
+if (file_exists($moviesStorageFile)) {
+    $moviesData = json_decode(file_get_contents($moviesStorageFile), true) ?? [];
+}
+
+$db = new PDO('mysql:host=localhost;dbname=Movie', $user, $pass);
+
+$select = 'SELECT source_id FROM movies WHERE source_id = :source_id';
+
+$insert = 'INSERT INTO movies (source_id, name, link, release_year, rating, poster, description)
+              VALUES (:source_id, :name, :link, :release_year, :rating, :poster, :description)';
+
+foreach ($moviesData as $movie) {
+
+    $sqlSelect = $db->prepare($select);
+
+    $sqlSelect->bindParam(':source_id', $movie['sourceId']);
+
+    $sqlSelect->execute();
+
+    if ($sqlSelect->fetchColumn()) {
+        continue;
+    }
+
+    $sqlInsert = $db->prepare($insert);
+
+    $sqlInsert->bindParam(':source_id', $movie['sourceId']);
+    $sqlInsert->bindParam(':name', $movie['name']);
+    $sqlInsert->bindParam(':link', $movie['link']);
+    $sqlInsert->bindParam(':release_year', $movie['release_year']);
+    $sqlInsert->bindParam(':rating', $movie['rating']);
+    $sqlInsert->bindParam(':poster', $movie['poster']);
+    $sqlInsert->bindParam(':description', $movie['description']);
+
+    $sqlInsert->execute();
+}
+
+$db = null;
---
+++ migrations/init.sql
@@ -0,0 +1,44 @@
+
+CREATE TABLE IF NOT EXISTS movies
+(
+    id           BIGINT AUTO_INCREMENT NOT NULL,
+    source_id    VARCHAR(255)          NOT NULL,
+    name         VARCHAR(255)          NOT NULL,
+    link         VARCHAR(255)          NULL,
+    release_year INT                   NULL,
+    rating       FLOAT                 NULL,
+    poster       VARCHAR(255)          NULL,
+    description  TEXT                  NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS genres
+(
+    id   BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50)           NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS movie_genre
+(
+    movie_id BIGINT NOT NULL,
+    genre_id BIGINT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE CASCADE,
+    UNIQUE (movie_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS crew_members
+(
+    id        BIGINT       NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS movie_crew_member
+(
+    movie_id       BIGINT NOT NULL,
+    crew_member_id BIGINT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE CASCADE,
+    FOREIGN KEY (crew_member_id) REFERENCES crew_members (id) ON DELETE CASCADE
+);
